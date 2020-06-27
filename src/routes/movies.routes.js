5fdd3e4ecd419335ed22453c83adb20c32318268
JairKevinFG <jkfloresg@uni.pe>
2020-06-27 06:04:55 -0500

    schema con join

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express')
-
+const joi = require('@hapi/joi')
+const {movieIdSchema,createMovieSchema,updateMovieSchema} = require('../utils/schemas/movies')
+const validationHandler = require('../utils/middleware/validationHandler')
 const router = Router()
 
 const {  renderMovies,renderMovie , createMovie,updateMovie,deleteMovie } = require('../controllers/movies.controller')
@@ -8,16 +10,17 @@ const {  renderMovies,renderMovie , createMovie,updateMovie,deleteMovie } = requ
 router.get('/', renderMovies)
 
 // Render movie
-router.get('/:id', renderMovie)
+router.get('/:id', validationHandler(joi.object({id: movieIdSchema}),'params'),renderMovie)
 
 // Create movie
-router.post('/new', createMovie)
+router.post('/new',validationHandler(joi.object({createMovieSchema})), createMovie)
 
 //Update movie
-router.put('/:id',  updateMovie)
+router.put('/:id', validationHandler(joi.object({id: movieIdSchema}),'params'),  validationHandler(joi.object({updateMovieSchema})), updateMovie)
 
 //Delete movie
-router.delete('/:id', deleteMovie)
+router.delete('/:id',validationHandler(joi.object({id: movieIdSchema}),'params'), deleteMovie)
 
 
 module.exports = router
+ 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
+const {logErrors,errorHandler,wrapErrors} = require('./utils/middleware/errorHandlers')
+const notFoundHandler= require('../src/utils/middleware/notFoundHandler')
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+
+//routes
 app.use('/movies',require('./routes/movies.routes'))
+
+//catch 404
+app.use(notFoundHandler)
+
+// Errors middleware
+app.use(logErrors)
+app.use(wrapErrors)
+app.use(errorHandler)
+
 
 
 module.exports = app;
